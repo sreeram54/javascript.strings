00da047f73fbb46f641b9b44f26395f3474a4481
sreeram54 <vaddisreeram54@gmail.com>
2022-08-17 00:34:37 +0530

    strings

---
+++ strings.js
@@ -0,0 +1,14 @@
+var name = 'Peter';
+var name2 = 'jack';
+
+var name3= 'The name are '+ name +'and'+ name2;
+console.log(name3);
+
+var message = `Hi, I'm ${name}.`;
+console.log(message);
+
+var str = "Good Morning!";
+console.log(str.length);  
+
+var result = 'a' < 'b';
+console.log(result); // true
